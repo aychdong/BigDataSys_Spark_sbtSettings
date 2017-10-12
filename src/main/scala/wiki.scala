53690ce006c463446acfbe319fc2ddf9dd287200
dongchen <dongchen@node-0.huanyu-a1.michigan-bigdata-pg0.wisc.cloudlab.us>
2017-10-11 21:19:46 -0500

    question2

--- src/main/scala/wiki.scala
+++ src/main/scala/wiki.scala
@@ -1,4 +1,3 @@
-
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkConf
@@ -11,10 +10,10 @@ def main(args: Array[String]): Unit={
 val spark_master_url = "spark://c220g1-030627.wisc.cloudlab.us:7077"
 val username = "dongchen"
 
-val config = new SparkConf().setAppName("wiki").setMaster(spark_master_url)
+val config = new SparkConf().setAppName("wiki").setMaster("local[4]")
 val sc = new SparkContext(config)
 val sqlContext = new SQLContext(sc)
-val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("/enwiki-20110115-pages-articles1.xml")
+val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("file:///users/dongchen/tmp/enwiki-20110115-pages-articles1.xml")
 val title_text_tmp = df.select("title", "revision.text._VALUE")
 //val text = title_text.select("_VALUE").toDF()
 val title_text = title_text_tmp.filter("_VALUE is not null")
@@ -24,7 +23,6 @@ val tmp = title_text.withColumn("newText", convert(title_text("_VALUE")))
 val new_title_text = tmp.select("title", "newText")
 val exploded = new_title_text.withColumn("newClo", explode(col("newText")))
 val final_res = exploded.select("title", "newClo")
-final_res.write.format("com.databricks.spark.csv").option("header", "false").option("delimiter","\t").mode("overwrite").save("/sb1.csv")
+final_res.write.format("com.databricks.spark.csv").option("header", "false").option("delimiter","\t").mode("overwrite").save("file:///users/dongchen/tmp/sb_q2.csv")
 }
 }
-
