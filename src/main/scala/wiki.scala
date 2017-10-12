df78f372d2a510f160711c72635e8e963be48b43
dongchen <dongchen@node-0.huanyu-a1.michigan-bigdata-pg0.wisc.cloudlab.us>
2017-10-11 21:24:41 -0500

    question5

--- src/main/scala/wiki.scala
+++ src/main/scala/wiki.scala
@@ -10,12 +10,11 @@ def main(args: Array[String]): Unit={
 val spark_master_url = "spark://c220g1-030627.wisc.cloudlab.us:7077"
 val username = "dongchen"
 
-val config = new SparkConf().setAppName("wiki").setMaster("local[4]")
+val config = new SparkConf().setAppName("wiki").setMaster(spark_master_url)
 val sc = new SparkContext(config)
 val sqlContext = new SQLContext(sc)
-val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("file:///users/dongchen/tmp/enwiki-20110115-pages-articles1.xml")
+val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("/enwiki-20110115-pages-articles_whole.xml")
 val title_text_tmp = df.select("title", "revision.text._VALUE")
-//val text = title_text.select("_VALUE").toDF()
 val title_text = title_text_tmp.filter("_VALUE is not null")
 val pattern = """\[\[(.*?)\]\]""".r
 val convert = udf((x: String) => pattern.findAllIn(x).toList.map(tmp=>tmp.substring(2, tmp.length-2)).filter(_.nonEmpty).map(tmp=>tmp.toLowerCase).map(tmp=>tmp.split("""\|""", -1)(0)).filter(_.nonEmpty).filterNot{tmp=>tmp.contains('#')}.filterNot{tmp=>(!tmp.startsWith("category:")) && tmp.contains(':')})
@@ -23,6 +22,7 @@ val tmp = title_text.withColumn("newText", convert(title_text("_VALUE")))
 val new_title_text = tmp.select("title", "newText")
 val exploded = new_title_text.withColumn("newClo", explode(col("newText")))
 val final_res = exploded.select("title", "newClo")
-final_res.write.format("com.databricks.spark.csv").option("header", "false").option("delimiter","\t").mode("overwrite").save("file:///users/dongchen/tmp/sb_q2.csv")
+final_res.write.format("com.databricks.spark.csv").option("header", "false").option("delimiter","\t").mode("overwrite").save("/sb_whole.csv")
 }
 }
+
