c99a522aab9c5bfab86ec2aa7adcc0565d8066f1
dongchen <dongchen@node-0.huanyu-a1.michigan-bigdata-pg0.wisc.cloudlab.us>
2017-10-11 21:11:01 -0500

    questin3

--- src/main/scala/wiki.scala
+++ src/main/scala/wiki.scala
@@ -1,19 +1,30 @@
+
+import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.explode
 
 object wiki{
 def main(args: Array[String]): Unit={
-val spark_master_url = "spark://c220g1-030818.wisc.cloudlab.us:7077"
+val spark_master_url = "spark://c220g1-030627.wisc.cloudlab.us:7077"
 val username = "dongchen"
 
-val config = new SparkConf().setAppName("task2").setMaster(spark_master_url)
+val config = new SparkConf().setAppName("wiki").setMaster(spark_master_url)
 val sc = new SparkContext(config)
 val sqlContext = new SQLContext(sc)
-val df = sqlContext.read
-	.format("com.databricks.spark.xml")
-	.option("rowTag", "page")
-	.load("/enwiki-20110115-pages-articles_whole.xml")
-df.printSchema()
+val df = sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "page").load("/enwiki-20110115-pages-articles1.xml")
+val title_text_tmp = df.select("title", "revision.text._VALUE")
+//val text = title_text.select("_VALUE").toDF()
+val title_text = title_text_tmp.filter("_VALUE is not null")
+val pattern = """\[\[(.*?)\]\]""".r
+val convert = udf((x: String) => pattern.findAllIn(x).toList.map(tmp=>tmp.substring(2, tmp.length-2)).filter(_.nonEmpty).map(tmp=>tmp.toLowerCase).map(tmp=>tmp.split("""\|""", -1)(0)).filter(_.nonEmpty).filterNot{tmp=>tmp.contains('#')}.filterNot{tmp=>(!tmp.startsWith("category:")) && tmp.contains(':')})
+val tmp = title_text.withColumn("newText", convert(title_text("_VALUE")))
+val new_title_text = tmp.select("title", "newText")
+val exploded = new_title_text.withColumn("newClo", explode(col("newText")))
+val final_res = exploded.select("title", "newClo")
+final_res.write.format("com.databricks.spark.csv").option("header", "false").option("delimiter","\t").mode("overwrite").save("/sb1.csv")
 }
 }
+
--- task2.sbt
+++ task2.sbt
@@ -4,6 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
 libraryDependencies += "com.databricks" % "spark-xml_2.11" % "0.4.1"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.0.1"
